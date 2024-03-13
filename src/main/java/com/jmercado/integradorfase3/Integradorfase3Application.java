1d94481f69df7acf4fb9a3b2ee08ef7ddba890c1
Juan Mercado <juan.mercado1986@gmail.com>
2024-03-13 12:18:52 -0300

    Primer commit

---
+++ src/main/java/com/jmercado/integradorfase3/Integradorfase3Application.java
@@ -0,0 +1,13 @@
+package com.jmercado.integradorfase3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Integradorfase3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Integradorfase3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jmercado/integradorfase3/Integradorfase3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmercado.integradorfase3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Integradorfase3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
